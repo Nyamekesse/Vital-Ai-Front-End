c648230a638791ca0dfb0cc13dbb034359b8a21f
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2023-07-26 16:54:06 +0000

    added loading state component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
+import { useState } from 'react'
+import Loading from './components/Loading'
 import { LogInPage, MainLayout, SignUpPage } from './pages'
 
 function App() {
+  const [open] = useState(false)
+  // const handleClose = () => {
+  //   setOpen(false)
+  // }
+  // const handleOpen = () => {
+  //   setOpen(true)
+  // }
   return (
     <div>
       {/* <LogInPage /> */}
       {/* <SignUpPage /> */}
       <MainLayout />
+      <Loading open={open} />
     </div>
   )
 }
---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,18 @@
+import Backdrop from '@mui/material/Backdrop'
+import CircularProgress from '@mui/material/CircularProgress'
+import Button from '@mui/material/Button'
+import { useState } from 'react'
+
+export default function Loading({ open }: { open: boolean }) {
+  return (
+    <div>
+      <Backdrop
+        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={open}
+        // onClick={handleClose}
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>
+    </div>
+  )
+}
