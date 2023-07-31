cef4a0fc95d2dae7ce5325701575caa9a5d321fc
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2023-07-31 15:58:45 +0000

    fixed not login redirect problem

--- src/pages/MainLayout/index.tsx
+++ src/pages/MainLayout/index.tsx
@@ -1,53 +1,45 @@
 import { useEffect, useState } from 'react';
-import { Outlet } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
+import { useCookies } from 'react-cookie';
 import TopBar from './components/TopBar';
 import BottomBar from './components/BottomBar';
 import { getStoredUser } from '../../user-storage';
 import { InfoResponse } from '../../types';
 
-function RenderViews() {
+export default function MainLayout() {
+  const [cookies] = useCookies(['vital_ai_token']);
+  const [storedUser, setStoredUser] = useState<InfoResponse>(getStoredUser());
   const [bottomNavHeight, setBottomNavHeight] = useState(0);
   const [topNavHeight, setTopNavHeight] = useState(0);
-
   useEffect(() => {
     setBottomNavHeight(
-      (document.querySelector('.bottom-nav') as HTMLElement).offsetHeight,
+      (document.querySelector('.bottom-nav') as HTMLElement)?.offsetHeight,
     );
     setTopNavHeight(
-      (document.querySelector('.top-nav') as HTMLElement).offsetHeight,
+      (document.querySelector('.top-nav') as HTMLElement)?.offsetHeight,
     );
-  }, []);
-
-  return (
-    <div style={{ marginBottom: bottomNavHeight, marginTop: topNavHeight }}>
-      <Outlet />
-    </div>
-  );
-}
-
-export default function MainLayout() {
-  const [storedUser, setStoredUser] = useState<InfoResponse>(getStoredUser());
-  useEffect(() => {
     if (!storedUser) {
       setStoredUser(getStoredUser());
     }
-  }, [storedUser]);
-  return (
+  }, [cookies, storedUser]);
+  return cookies.vital_ai_token && storedUser ? (
     <div className="flex flex-col overflow-x-hidden">
       <div className="top-nav fixed left-0 right-0 z-30">
         <TopBar
-          firstName={storedUser?.firstName as string}
-          lastName={storedUser?.lastName as string}
-          displayPicture={storedUser?.displayPicture as string}
+          firstName={storedUser?.firstName}
+          lastName={storedUser?.lastName}
+          displayPicture={storedUser?.displayPicture}
         />
       </div>
-      <div>
-        <RenderViews />
+      <div style={{ marginBottom: bottomNavHeight, marginTop: topNavHeight }}>
+        <Outlet />
       </div>
 
       <div className="bottom-nav fixed bottom-0 left-0 right-0 z-30">
         <BottomBar />
       </div>
     </div>
+  ) : (
+    <Navigate to="/log-in" replace />
   );
 }
