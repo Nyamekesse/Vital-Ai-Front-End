5aa77a60abc8f5cdd784ce404b0eb00dee8c106b
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2023-08-03 20:54:32 +0000

    refactoring code

--- src/axios-instance/index.ts
+++ src/axios-instance/index.ts
@@ -1,6 +1,5 @@
 import axios, { AxiosRequestConfig } from 'axios';
-import { redirect } from 'react-router-dom';
-import { toast } from 'react-toastify';
+
 import { url } from '../shared/constants';
 
 const config: AxiosRequestConfig = {
--- src/sockets/clientSocket.ts
+++ src/sockets/clientSocket.ts
@@ -2,6 +2,8 @@ import { io, Socket } from 'socket.io-client';
 
 let socket: Socket | null = null;
 export const socketServerConnection = (token: string) => {
+  console.log(token);
+
   socket = io('http://localhost:5000', {
     withCredentials: true,
     path: '/s',
