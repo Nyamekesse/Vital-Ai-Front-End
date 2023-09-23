6dc983951c78cb4047d1b37a9ed556b3d5576286
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2023-09-23 20:07:37 +0000

    refactored code

--- src/axios-instance/index.ts
+++ src/axios-instance/index.ts
@@ -2,16 +2,20 @@ import axios, { AxiosRequestConfig } from 'axios';
 import { toast } from 'react-toastify';
 
 const config: AxiosRequestConfig = {
-  baseURL: import.meta.env.VITE_REACT_APP_SERVER_URL_1,
+  baseURL: 'https://vital-ai-node-server.onrender.com/api/',
   withCredentials: true,
 };
+// const config: AxiosRequestConfig = {
+//   baseURL: import.meta.env.VITE_REACT_APP_SERVER_URL_1,
+//   withCredentials: true,
+// };
 const axiosInstance = axios.create(config);
 
 axiosInstance.interceptors.response.use(
   (response) => response,
   (error) => {
     if (error.response?.status === 401) {
-      window.location.reload();
+      // window.location.reload();
       toast.warning('Session expired please log in again');
     }
     return Promise.reject(error);
