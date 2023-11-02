97f61edbda63427cff2b164e29019ff1e7c2aa4f
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2023-11-02 22:24:35 +0000

    fixed unused variable

--- src/pages/MainLayout/index.tsx
+++ src/pages/MainLayout/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Navigate, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import { fetchCookie } from '../../utils/fetchCookie';
 import {
   socketServerConnection,
