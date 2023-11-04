10124c9e4f741bca8f8055709d1104799edd7983
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2023-11-04 00:58:36 +0000

    modified axios interceptors

--- src/axios-instance/index.ts
+++ src/axios-instance/index.ts
@@ -1,5 +1,4 @@
 import axios, { AxiosRequestConfig } from 'axios';
-import { toast } from 'react-toastify';
 
 const config: AxiosRequestConfig = {
   baseURL: import.meta.env.VITE_REACT_APP_SERVER_URL_1,
@@ -10,9 +9,6 @@ const axiosInstance = axios.create(config);
 axiosInstance.interceptors.response.use(
   (response) => response,
   (error) => {
-    if (error.response?.status === 401) {
-      toast.warning('Session expired please log in again');
-    }
     return Promise.reject(error);
   },
 );
